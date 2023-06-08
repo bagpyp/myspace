a21953d5c4dfabd5ff3f8e89eea1d3de98538857
robertcunningham <robertcunningham@ford.com>
2023-06-08 10:31:51 -0700

    build signin/out components with avatar, configure requred routing for next/Image component

---
+++ components/buttons.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function SignInButton() {
+  const { data: session, status } = useSession();
+  console.log(session, status);
+
+  if (status === 'loading') {
+    return <>...</>;
+  }
+
+  if (status === 'authenticated') {
+    return (
+      <Link href={`/dashboard`}>
+        <Image
+          src={session?.user?.image ?? '/mememan.webp'}
+          width={32}
+          height={32}
+          alt={'Your Name'}
+        />
+      </Link>
+    );
+  }
+
+  return <button></button>;
+}
+
+export function SignOuButton() {
+  return (
+    <button
+      onClick={() => {
+        signOut();
+      }}
+    >
+      Sign Out
+    </button>
+  );
+}
