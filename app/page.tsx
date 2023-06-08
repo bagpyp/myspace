04bdff12eb7c077c3d28f1ea7be370b4fb05d2f5
robertcunningham <robertcunningham@ford.com>
2023-06-08 10:54:52 -0700

    protect route with redirect to signin on void session

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import Image from 'next/image';
-import styles from '@/app/page.module.css';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return <main className={styles.main}></main>;
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
+  return <main></main>;
 }
