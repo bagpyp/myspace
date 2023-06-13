b4251aa1b48a88907ed663826c9a3fb4d01fd14d
robertcunningham <robertcunningham@ford.com>
2023-06-13 16:50:26 -0700

    create route for profile mutation (PUT /user)

---
+++ app/api/user/route.ts
@@ -0,0 +1,18 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { redirect } from 'next/navigation';
+import { prisma } from '@/lib/prisma';
+
+export default async function PUT(req: Request) {
+  const session = await getServerSession(authOptions);
+  const currentUserEmail = session?.user?.email!;
+
+  const data = await req.json();
+  data.age = Number(data.age);
+
+  const user = await prisma.user.update({
+    where: { email: currentUserEmail },
+    // TODO: validate data
+    data
+  });
+}
