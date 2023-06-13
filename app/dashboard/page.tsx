ea8830cb623d32583246c718d514c215f3bf62da
robertcunningham <robertcunningham@ford.com>
2023-06-13 16:34:42 -0700

    build a component and put it on the couch

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
 import { prisma } from '@/lib/prisma';
+import { ProfileForm } from '@/app/dashboard/ProfileForm';
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
